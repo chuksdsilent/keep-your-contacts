abd74186f92e6c6cc71c84c90106e22820b8b070
Oshabz <chuksdsilent@gmail.com>
2020-04-22 17:04:34 +0100

    Changed port number

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 8800;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}..`))
