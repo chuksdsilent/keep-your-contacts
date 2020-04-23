a83ee7e1955229518f59e455203f1b797f353705
Oshabz <chuksdsilent@gmail.com>
2020-04-23 13:25:43 +0100

    Update Server

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ if (process.env.NODE_ENV === 'production') {
 // app.listen(PORT, () => console.log(`Server started on port ${PORT}..`))
 var server_port = process.env.YOUR_PORT || process.env.PORT || 7500;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function () {
+app.listen(server_port, server_host, function () {
     console.log('Listening on port %d', server_port);
 });
